4d008538da66c5e49a85fee5ec3678541606ae24
atif9281 <atiff.khan92810000@gmail.com>
2024-07-02 15:30:58 +0500

    some changes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
